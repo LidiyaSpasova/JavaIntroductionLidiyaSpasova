66891f3a3ff1b02a9be0fa53db57a8bc0dacd0fe
LidiyaSpasova <lidia.spasova12@gmail.com>
2023-08-22 19:13:02 +0300

    added HomeworkFirst

---
+++ src/main/java/Homework/HomeworkFirst.java
@@ -0,0 +1,29 @@
+//Напишете програма, която:
+//Чете за име на потребител
+//Чете за любимо хоби на потребителя
+//Изписва в конзолата подходящо съобщение за потребителя и хобито
+
+
+package Homework;
+
+import java.util.Scanner;
+
+public class HomeworkFirst {
+
+        public static void main(String [] args){
+
+            System.out.println("Please enter your name:");
+            Scanner scanner = new Scanner(System.in);
+
+            String name = scanner.nextLine();
+
+            System.out.println("What is your hobby?");
+
+            String hobby = scanner.nextLine();
+
+            System.out.println("Nice to meet you," + name + ". Your hobby is:" + hobby);
+
+
+        }
+    }
+

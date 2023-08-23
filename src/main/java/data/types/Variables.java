32a133ae7473a7e62362aa1e791994de2ae7597f
LidiyaSpasova <lidia.spasova12@gmail.com>
2023-08-23 23:12:18 +0300

    lecture 5

---
+++ src/main/java/data/types/Variables.java
@@ -0,0 +1,42 @@
+package data.types;
+
+public class Variables {
+    //static variable
+    static String staticString;
+    //instance variables
+
+    byte age;
+
+    public static void main(String [] args) {
+        //local variables
+        //primitive data types
+
+        byte age = 20;
+        short workingDaysPerYear = 200;
+        int secondInWorkingWeek = 345354;
+        long secondInAnYear = 63454455;
+        float accountBalance = 23423423.56f;
+        double resultOfCalculation = 0.0;
+        boolean amIRich = false;
+        char specialSymbol = '&';
+
+        //referent data types
+        String firstName = "Ibro";
+        String firstNAME = "Petar";
+    }
+
+    public void printAge(){
+        System.out.println("Age is:" + age + ".");
+    }
+
+    public void printCanDrink(){
+        if (age >=18) {
+            System.out.printf("Yes! Because the age is: %d%n", age);
+        }else {
+            System.out.println("Sorry :(");
+        }
+        }
+    }
+
+
+
--- src/main/java/demo/first/FirstDemo.java
+++ src/main/java/demo/first/FirstDemo.java
@@ -7,20 +7,22 @@ import java.util.Scanner;
 public class FirstDemo {
 
     //method
-    public static void main(String [] args){
-        System.out.println("Hello Java");
+    public static void main (String [] args){
+        System.out.println("Hello JAVA");
 
         System.out.println("Please enter your name:");
-        Scanner scanner = new Scanner(System.in); //Creating a Scanner variable
+        Scanner scanner = new Scanner(System.in); // Creating a Scanner variable
 
-        String name = scanner.nextLine(); //Read user input
+        String name = scanner.nextLine();// Read user input
 
         System.out.println("How old are you?");
 
         int age = scanner.nextInt();
 
-        System.out.println("Nice to meet you," + name + ". Your age is:" + age);
+        System.out.println("Nice to meet you, " + name + ". Your age is: " + age);
 
-
+        /* multiline
+        comments
+         */
     }
 }
